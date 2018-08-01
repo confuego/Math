44c52905beed3c2d068afc4fc805f597910aeecf
Wesley <wes.chappell10@gmail.com>
2018-08-01 12:31:44 -0400

    - change package name - remove hot load - add matrix multiply -> still some perf tweaks - make data -> _data and private

--- src/matrix3.ts
+++ src/matrix3.ts
@@ -1,6 +1,6 @@
 export class Matrix3 {
 
-	public data: Float64Array | Float32Array = new Float32Array(9)
+	private _data: Float64Array | Float32Array = new Float32Array(9)
 
 	constructor(..._rows: Array<Float32Array | Float64Array>) {
 		if(Array.isArray(_rows)
@@ -11,48 +11,70 @@ export class Matrix3 {
 	}
 
 	private _buildArray(rows: Array<Float32Array | Float64Array>) {
-		this.data[0] = rows[0][0]
-		this.data[1] = rows[0][1]
-		this.data[2] = rows[0][2]
-		this.data[3] = rows[1][0]
-		this.data[4] = rows[1][1]
-		this.data[5] = rows[1][2]
-		this.data[6] = rows[2][0]
-		this.data[7] = rows[2][1]
-		this.data[8] = rows[2][2]
+		this._data[0] = rows[0][0]
+		this._data[1] = rows[0][1]
+		this._data[2] = rows[0][2]
+		this._data[3] = rows[1][0]
+		this._data[4] = rows[1][1]
+		this._data[5] = rows[1][2]
+		this._data[6] = rows[2][0]
+		this._data[7] = rows[2][1]
+		this._data[8] = rows[2][2]
 	}
 
 	public add(mat: Matrix3, dest: Matrix3 = new Matrix3()): Matrix3 {
-		dest.data[0] = this.data[0] + mat.data[0];
-		dest.data[1] = this.data[1] + mat.data[1];
-		dest.data[2] = this.data[2] + mat.data[2];
-		dest.data[3] = this.data[3] + mat.data[3];
-		dest.data[4] = this.data[4] + mat.data[4];
-		dest.data[5] = this.data[5] + mat.data[5];
-		dest.data[6] = this.data[6] + mat.data[6];
-		dest.data[7] = this.data[7] + mat.data[7];
-		dest.data[8] = this.data[8] + mat.data[8];
+		dest._data[0] = this._data[0] + mat._data[0];
+		dest._data[1] = this._data[1] + mat._data[1];
+		dest._data[2] = this._data[2] + mat._data[2];
+		dest._data[3] = this._data[3] + mat._data[3];
+		dest._data[4] = this._data[4] + mat._data[4];
+		dest._data[5] = this._data[5] + mat._data[5];
+		dest._data[6] = this._data[6] + mat._data[6];
+		dest._data[7] = this._data[7] + mat._data[7];
+		dest._data[8] = this._data[8] + mat._data[8];
 		return dest;
 	}
 
 	public sub(mat: Matrix3, dest: Matrix3 = new Matrix3()): Matrix3 {
-		dest.data[0] = this.data[0] - mat.data[0];
-		dest.data[1] = this.data[1] - mat.data[1];
-		dest.data[2] = this.data[2] - mat.data[2];
-		dest.data[3] = this.data[3] - mat.data[3];
-		dest.data[4] = this.data[4] - mat.data[4];
-		dest.data[5] = this.data[5] - mat.data[5];
-		dest.data[6] = this.data[6] - mat.data[6];
-		dest.data[7] = this.data[7] - mat.data[7];
-		dest.data[8] = this.data[8] - mat.data[8];
+		dest._data[0] = this._data[0] - mat._data[0];
+		dest._data[1] = this._data[1] - mat._data[1];
+		dest._data[2] = this._data[2] - mat._data[2];
+		dest._data[3] = this._data[3] - mat._data[3];
+		dest._data[4] = this._data[4] - mat._data[4];
+		dest._data[5] = this._data[5] - mat._data[5];
+		dest._data[6] = this._data[6] - mat._data[6];
+		dest._data[7] = this._data[7] - mat._data[7];
+		dest._data[8] = this._data[8] - mat._data[8];
+		return dest;
+	}
+
+	public mul(mat: Matrix3, dest: Matrix3 = new Matrix3()): Matrix3 {
+		dest._data[0] = this._data[0] * mat._data[0] + this._data[1] * mat._data[3] + this._data[2] * mat._data[6];
+		dest._data[3] = this._data[3] * mat._data[0] + this._data[4] * mat._data[3] + this._data[5] * mat._data[6];
+		dest._data[6] = this._data[6] * mat._data[0] + this._data[7] * mat._data[3] + this._data[8] * mat._data[6];
+
+		dest._data[1] = this._data[0] * mat._data[1] + this._data[1] * mat._data[4] + this._data[2] * mat._data[7];
+		dest._data[4] = this._data[3] * mat._data[1] + this._data[4] * mat._data[4] + this._data[5] * mat._data[7];
+		dest._data[7] = this._data[6] * mat._data[1] + this._data[7] * mat._data[4] + this._data[8] * mat._data[7];
+
+		dest._data[2] = this._data[0] * mat._data[2] + this._data[1] * mat._data[5] + this._data[2] * mat._data[8];
+		dest._data[5] = this._data[3] * mat._data[2] + this._data[4] * mat._data[5] + this._data[5] * mat._data[8];
+		dest._data[8] = this._data[6] * mat._data[2] + this._data[7] * mat._data[5] + this._data[8] * mat._data[8];
 		return dest;
 	}
 
 	public get(row: number, col: number): number {
-		return this.data[row * col + col]
+		return this._data[row * col + col]
 	}
 
 	public toString(): string {
-		return '';
+		let display = '';
+
+		for(let i = 0; i < this._data.length; i++) {
+			display += (i % 3 === 0) ? '\n' : ',';
+			display += this._data[i].toString()
+		}
+
+		return display;
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.ts',
-	mode: "production",
+	mode: "development",
 	devServer: {
-		contentBase: './',
-		hotOnly: true
+		contentBase: './'
 	},
   module: {
     rules: [
